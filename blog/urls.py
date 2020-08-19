dadfa28d66caeb32e2986df45c57b62cd5652013
Fahad-CSE16 <Fahad-CSE16>
2020-08-19 20:12:20 +0600

    4th tutorial done and it covered request and its resoponses

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import *
-
+from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
-    path('',post_list),
+    path('all/',post_list),
     path('<int:pk>/', post_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- blog/views.py
+++ blog/views.py
@@ -1,53 +1,55 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from .models import Post, Category
 from . serializers import PostSerializer, CategorySerializer
 # Create your views here.
 
 
-@csrf_exempt
-def post_list(request):
+@api_view(['GET', 'POST'])
+def post_list(request, format=None):
     """
     List all code posts, or create a new post.
     """
     if request.method == 'GET':
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = PostSerializer(data=data)
+        serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return  Response(serializer.data, status=status.HTTP_201_CREATED)
+        return  Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
-def post_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def post_detail(request, pk, format=None):
     """
     Retrieve, update or delete a code post.
     """
     try:
         post = Post.objects.get(pk=pk)
     except Post.DoesNotExist:
-        return HttpResponse(status=404)
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = PostSerializer(post)
-        return JsonResponse(serializer.data)
+        return  Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = PostSerializer(post, data=data)
+        
+        serializer = PostSerializer(post, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return  Response(serializer.data)
+        return  Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         post.delete()
-        return HttpResponse(status=204)
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
