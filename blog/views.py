0884192ca766d0e4db43a22f50ed2e0183e4032d
Fahad-CSE16 <Fahad-CSE16>
2020-08-19 20:47:42 +0600

    5th tutorial done with generic class based views

--- blog/views.py
+++ blog/views.py
@@ -6,55 +6,69 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
+from rest_framework import generics
 from .models import Post, Category
 from . serializers import PostSerializer, CategorySerializer
 
 
 # Create your views here.
+class PostList(generics.ListCreateAPIView):
+    queryset= Post.objects.all()
+    serializer_class = PostSerializer
 
-
-class PostList(APIView):
-    def get(self, request, format=None):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class PostDetail(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
 
 
-class PostDetail(APIView):
-    """
-    Retrieve, update or delete a code post.
-    """
-    def get_object(self, pk):
-        try:
-            return Post.objects.get(pk=pk)
-        except Post.DoesNotExist:
-            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
-    def get(self, request, pk , format=None):
-        post= self.get_object(pk)
-        serializer = PostSerializer(post)
-        return  Response(serializer.data) 
 
-    def put(self, request, pk, format=None):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return  Response(serializer.data)
-        return  Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk, format=None):
-        post = self.get_object(pk)
-        post.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+
+# class PostList(APIView):
+#     def get(self, request, format=None):
+#         posts = Post.objects.all()
+#         serializer = PostSerializer(posts, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request, format=None):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+# class PostDetail(APIView):
+#     """
+#     Retrieve, update or delete a code post.
+#     """
+#     def get_object(self, pk):
+#         try:
+#             return Post.objects.get(pk=pk)
+#         except Post.DoesNotExist:
+#             return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+#     def get(self, request, pk , format=None):
+#         post= self.get_object(pk)
+#         serializer = PostSerializer(post)
+#         return  Response(serializer.data) 
+
+#     def put(self, request, pk, format=None):
+#         post = self.get_object(pk)
+#         serializer = PostSerializer(post, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return  Response(serializer.data)
+#         return  Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request, pk, format=None):
+#         post = self.get_object(pk)
+#         post.delete()
+#         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
 
 
 # @api_view(['GET', 'POST'])
