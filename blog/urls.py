2428549488fe15afd5fc86ae47aa20b06e9ca81b
Fahad-CSE16 <Fahad-CSE16>
2020-08-19 20:41:28 +0600

    5th tutorial done with simple class based views

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
-    path('all/',post_list),
-    path('<int:pk>/', post_detail),
+    path('all/',PostList.as_view()),
+    path('<int:pk>/', PostDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
