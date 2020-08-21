a4355f1624a4b466bddc1608f51cb35452455883
Fahad-CSE16 <Fahad-CSE16>
2020-08-21 08:07:15 +0600

    different nessted relationships added

--- blog/serializers.py
+++ blog/serializers.py
@@ -3,22 +3,21 @@ from django.contrib.auth.models import User
 from . models import Post , Category
 
 
-class UserSerializer(serializers.ModelSerializer):
-    posts = serializers.PrimaryKeyRelatedField(many=True, queryset=Post.objects.all())
+class UserSerializer1(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'posts']
-
-
+        fields = ['id', 'username', 'email']
 
 class PostSerializer1(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-    # category = serializers.HyperlinkedRelatedField(many=False, read_only=True, view_name="single-category")
-    # category= serializers.StringRelatedField(many=False)
-    # category= serializers.SlugRelatedField(many=False, read_only=True, slug_field='title')
+    owner = UserSerializer1(many=False, read_only=True)
     class Meta:
         model = Post
-        fields = ['owner','title', 'body']
+        fields = ['title', 'body', 'owner']
+
+class PostSerializer2(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['title', 'body','created_at']
 
 class CategorySerializer(serializers.ModelSerializer):
     posts= PostSerializer1(many= True, read_only=True, source='post_set')
@@ -26,14 +25,19 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ['title',  'created_at', 'posts']
 
-
 class PostSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
+    # owner = serializers.ReadOnlyField(source='owner.username')
+    owner=UserSerializer1(many=False, read_only=True )
     # category = serializers.HyperlinkedRelatedField(many=False, read_only=True, view_name="single-category")
     # category= serializers.StringRelatedField(many=False)
-    category = serializers.SlugRelatedField(
-        many=False, read_only=True, slug_field='title')
-
+    # category = serializers.SlugRelatedField(many=False, read_only=True, slug_field='title')
     class Meta:
         model = Post
         fields = ['owner', 'title', 'body', 'category']
+
+class UserSerializer(serializers.ModelSerializer):
+    # posts = serializers.PrimaryKeyRelatedField(many=True, queryset=Post.objects.all())
+    posts=PostSerializer2(many=True, read_only=True)
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'posts']
